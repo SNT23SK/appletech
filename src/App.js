ec7baa67055e3a964e8dc25d0008265e5ae7d9ad
Константин Скоробогатов <snt23sk@gmail.com>
2021-08-19 13:55:23 +0400

    added props in logo

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import home_icon from "./assets/img/home.svg";
 import like_icon from "./assets/img/like.svg";
 import busket_icon from "./assets/img/busket.svg";
-import small_like_icon from "./assets/img/small_like.svg";
-import macbook_png from "./assets/img/mac.png";
+import ProductBlock from "./components/ProductBlock";
 import "./App.css";
 
-function App() {
+function App(props) {
 	return (
 		<>
 			<div className="wrapper-header">
 				<header className="header">
 					<div className="header__logo">
-						<p className="logo">AppleTech</p>
+						<p className="logo">{props.logo}</p>
 					</div>
 					<div className="header__menu">
 						<ul className="list header__list">
@@ -42,141 +41,14 @@ function App() {
 			</div>
 			<div className="wrapper-content">
 				<div className="content__goods">
-					<div className="content__block">
-						<img className="block__like_btn" src={small_like_icon} alt="like" />
-						<img className="block__main_img" src={macbook_png} alt="macbook" />
-						<p className="block__description">
-							Ноутбук Apple MacBook Pro 13" M1 256GB 2020 (MYDA2) Silver
-						</p>
-						<div className="content__block_buy">
-							<div className="block__price">
-								<p className="discount">97000 руб</p>
-								<p className="price">85000 руб</p>
-							</div>
-							<div>
-								<button className="buy_btn">Купить</button>
-							</div>
-						</div>
-					</div>
-
-					<div className="content__block">
-						<img className="block__like_btn" src={small_like_icon} alt="like" />
-						<img className="block__main_img" src={macbook_png} alt="macbook" />
-						<p className="block__description">
-							Ноутбук Apple MacBook Pro 13" M1 256GB 2020 (MYDA2) Silver
-						</p>
-						<div className="content__block_buy">
-							<div className="block__price">
-								<p className="discount">97000 руб</p>
-								<p className="price">85000 руб</p>
-							</div>
-							<div>
-								<button className="buy_btn">Купить</button>
-							</div>
-						</div>
-					</div>
-
-					<div className="content__block">
-						<img className="block__like_btn" src={small_like_icon} alt="like" />
-						<img className="block__main_img" src={macbook_png} alt="macbook" />
-						<p className="block__description">
-							Ноутбук Apple MacBook Pro 13" M1 256GB 2020 (MYDA2) Silver
-						</p>
-						<div className="content__block_buy">
-							<div className="block__price">
-								<p className="discount">97000 руб</p>
-								<p className="price">85000 руб</p>
-							</div>
-							<div>
-								<button className="buy_btn">Купить</button>
-							</div>
-						</div>
-					</div>
-
-					<div className="content__block">
-						<img className="block__like_btn" src={small_like_icon} alt="like" />
-						<img className="block__main_img" src={macbook_png} alt="macbook" />
-						<p className="block__description">
-							Ноутбук Apple MacBook Pro 13" M1 256GB 2020 (MYDA2) Silver
-						</p>
-						<div className="content__block_buy">
-							<div className="block__price">
-								<p className="discount">97000 руб</p>
-								<p className="price">85000 руб</p>
-							</div>
-							<div>
-								<button className="buy_btn">Купить</button>
-							</div>
-						</div>
-					</div>
-
-					<div className="content__block">
-						<img className="block__like_btn" src={small_like_icon} alt="like" />
-						<img className="block__main_img" src={macbook_png} alt="macbook" />
-						<p className="block__description">
-							Ноутбук Apple MacBook Pro 13" M1 256GB 2020 (MYDA2) Silver
-						</p>
-						<div className="content__block_buy">
-							<div className="block__price">
-								<p className="discount">97000 руб</p>
-								<p className="price">85000 руб</p>
-							</div>
-							<div>
-								<button className="buy_btn">Купить</button>
-							</div>
-						</div>
-					</div>
-
-					<div className="content__block">
-						<img className="block__like_btn" src={small_like_icon} alt="like" />
-						<img className="block__main_img" src={macbook_png} alt="macbook" />
-						<p className="block__description">
-							Ноутбук Apple MacBook Pro 13" M1 256GB 2020 (MYDA2) Silver
-						</p>
-						<div className="content__block_buy">
-							<div className="block__price">
-								<p className="discount">97000 руб</p>
-								<p className="price">85000 руб</p>
-							</div>
-							<div>
-								<button className="buy_btn">Купить</button>
-							</div>
-						</div>
-					</div>
-
-					<div className="content__block">
-						<img className="block__like_btn" src={small_like_icon} alt="like" />
-						<img className="block__main_img" src={macbook_png} alt="macbook" />
-						<p className="block__description">
-							Ноутбук Apple MacBook Pro 13" M1 256GB 2020 (MYDA2) Silver
-						</p>
-						<div className="content__block_buy">
-							<div className="block__price">
-								<p className="discount">97000 руб</p>
-								<p className="price">85000 руб</p>
-							</div>
-							<div>
-								<button className="buy_btn">Купить</button>
-							</div>
-						</div>
-					</div>
-
-					<div className="content__block">
-						<img className="block__like_btn" src={small_like_icon} alt="like" />
-						<img className="block__main_img" src={macbook_png} alt="macbook" />
-						<p className="block__description">
-							Ноутбук Apple MacBook Pro 13" M1 256GB 2020 (MYDA2) Silver
-						</p>
-						<div className="content__block_buy">
-							<div className="block__price">
-								<p className="discount">97000 руб</p>
-								<p className="price">85000 руб</p>
-							</div>
-							<div>
-								<button className="buy_btn">Купить</button>
-							</div>
-						</div>
-					</div>
+					<ProductBlock></ProductBlock>
+					<ProductBlock></ProductBlock>
+					<ProductBlock></ProductBlock>
+					<ProductBlock></ProductBlock>
+					<ProductBlock></ProductBlock>
+					<ProductBlock></ProductBlock>
+					<ProductBlock></ProductBlock>
+					<ProductBlock></ProductBlock>
 				</div>
 			</div>
 		</>
